a5d96bfc41bec80f98daed922216962d35c7523d
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:34:32 +0100

    feat: export all entities

---
+++ apps/@tinie/dynamo/src/index.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export { client } from './client';
+export { retrieveLongUrl } from './functions/retrieve-long-url.function';
+export { insertShortUrl } from './functions/insert-short-url.function';
+export { updateUrlEntry } from './functions/update-url-entry.function';
+export { getUrlList } from './functions/get-url-list.function';
+export { getDbStatus } from './functions/get-db-status.function';
