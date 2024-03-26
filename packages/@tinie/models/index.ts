4096164889304428b3501409d94798fc02d96473
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:30:26 +0100

    feat: create package for models

---
+++ packages/@tinie/models/index.ts
@@ -0,0 +1,3 @@
+/** @format */
+
+export type { UrlModel } from './models/url.model';
---
+++ packages/@tinie/models/models/url.model.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export type UrlModel = {
+    short_url: string;
+    long_url: string;
+    created_at: Date;
+    visits: number;
+};
