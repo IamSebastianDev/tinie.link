96074fe90e17cc50eff934e2bf2ca638836bc002
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:35:00 +0100

    feat: implement primary range service dummy

---
+++ apps/@tinie/server/src/lib/range.service.ts
@@ -0,0 +1,32 @@
+/** @format */
+
+export const RangeService = () => {
+    let _range: number[] = [];
+    const getNewRangeFromZookeeper = async () => {
+        // Implement the fetch to the Zookeeper here
+        _range = Array(100000)
+            .fill(null)
+            .map((_, idx) => idx);
+    };
+
+    getNewRangeFromZookeeper();
+
+    return {
+        fetchRange: async () => {
+            return await getNewRangeFromZookeeper();
+        },
+        getNumber: async () => {
+            if (_range.length === 0) {
+                await getNewRangeFromZookeeper();
+            }
+
+            // To make the next url not to deterministic,
+            // we calculate a random index to remove from the
+            // range array, that will then be removed and returned
+            const randomIdx = Math.floor(Math.random() * _range.length) * 1;
+            const [accessed] = _range.splice(randomIdx);
+
+            return accessed;
+        },
+    };
+};
