5c2827a6807f4d41fbe84a5690eac9908ad40c3c
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:36:05 +0100

    refactor: add routes

--- apps/@tinie/server/src/api/api.router.ts
+++ apps/@tinie/server/src/api/api.router.ts
@@ -5,8 +5,15 @@ import { JSend } from '@elysia-plugin/jsend';
 import type { App } from '../bootstrap';
 import { CreateUrlController } from './create-url/create-url.controller';
 import { ShortUrlController } from './short-url/short-url.controller';
+import { UrlListController } from './url-list/url-list.controller';
 
 export const ApiRouter = (app: App) =>
     app.group('api/v1', (app) =>
-        app.use(JSend()).use(HealthController).use(CreateUrlController).use(ShortUrlController),
+        app
+            .use(JSend())
+            .use(HealthController)
+            .use(CreateUrlController)
+            .use(ShortUrlController)
+            .use(UrlListController)
+            .use((app) => app),
     );
