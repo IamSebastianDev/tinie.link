2b4a7e34a520aab13591faaebcf856334258c00d
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:35:24 +0100

    feat: implement create url module

--- apps/@tinie/server/src/api/create-url/create-url.controller.ts
+++ apps/@tinie/server/src/api/create-url/create-url.controller.ts
@@ -3,19 +3,30 @@
 import { t } from 'elysia';
 import type { App } from '../../bootstrap';
 import { CreateUrlService } from './create-url.service';
+import { Base62Service } from '../../lib/base62.service';
+import { RangeService } from '../../lib/range.service';
 
 export const CreateUrlController = (app: App) =>
     app.group('create-url/', (app) =>
         app
             .decorate({
                 CreateUrlService: CreateUrlService(),
+                Base62Service: Base62Service(),
+                RangeService: RangeService(),
             })
-            .post('/', async ({ CreateUrlService, body }) => await CreateUrlService.create(body.long_url), {
-                // validation
-                body: t.Object({
-                    long_url: t.String({
-                        description: 'Must be a valid url',
+            .post(
+                '/',
+                async ({ CreateUrlService, Base62Service, RangeService, body }) => {
+                    const short_url = Base62Service.convert(await RangeService.getNumber());
+                    return await CreateUrlService.create(body.long_url, short_url);
+                },
+                {
+                    // validation
+                    body: t.Object({
+                        long_url: t.String({
+                            description: 'The URL that is tiniefied.',
+                        }),
                     }),
-                }),
-            }),
+                },
+            ),
     );
--- apps/@tinie/server/src/api/create-url/create-url.service.ts
+++ apps/@tinie/server/src/api/create-url/create-url.service.ts
@@ -1,11 +1,34 @@
 /** @format */
 
+import { handleAsync } from '@iasd/handle-async';
+import { insertShortUrl } from '@tinie/dynamo';
+import type { UrlModel } from '@tinie/models';
+import { InternalServerError } from 'elysia';
+
 export const CreateUrlService = () => {
     return {
-        create: async (url: string) => {
-            return {
-                short_url: url,
-            };
+        create: async (long_url: string, short_url: string) => {
+            return await handleAsync(
+                async () => {
+                    const entity: UrlModel = { long_url, short_url, visits: 0, created_at: new Date() };
+
+                    return await insertShortUrl(entity);
+                },
+                {
+                    Ok: (response) => {
+                        if (!!response) {
+                            return {
+                                short_url: response.short_url,
+                            };
+                        }
+
+                        throw new Error('Short Url could not be created');
+                    },
+                    Err: (err) => {
+                        throw err;
+                    },
+                },
+            );
         },
     };
 };
