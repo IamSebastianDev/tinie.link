44d7c126faacd8a4b279aef16e5511e52283cfd6
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-26 23:33:04 +0100

    feat: add function to update visits count

---
+++ apps/@tinie/dynamo/src/functions/update-url-entry.function.ts
@@ -0,0 +1,33 @@
+/** @format */
+
+import { handleAsync } from '@iasd/handle-async';
+import type { UrlModel } from '@tinie/models';
+import { client } from '..';
+import { UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+
+export const updateUrlEntry = async (key: UrlModel['short_url']) => {
+    return await handleAsync(
+        async () => {
+            return await client.send(
+                new UpdateItemCommand({
+                    TableName: 'url',
+                    Key: {
+                        short_url: { S: key },
+                    },
+                    // Define an expression to add 1 to the visits field
+                    UpdateExpression: 'SET visits = visits + :inc',
+                    // Define the value for :inc variable in the expression
+                    ExpressionAttributeValues: {
+                        ':inc': { N: '1' },
+                    },
+                    // Return the updated item
+                    ReturnValues: 'UPDATED_NEW',
+                }),
+            );
+        },
+        {
+            Ok: () => true,
+            Err: (err) => false,
+        },
+    );
+};
