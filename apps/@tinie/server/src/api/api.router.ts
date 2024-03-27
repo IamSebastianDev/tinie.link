7af564d7ad04962cf1046412011602ce140cafbd
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-27 21:57:04 +0100

    docs: add documentation on api controller

--- apps/@tinie/server/src/api/api.router.ts
+++ apps/@tinie/server/src/api/api.router.ts
@@ -7,11 +7,19 @@ import { CreateUrlController } from './create-url/create-url.controller';
 import { ShortUrlController } from './short-url/short-url.controller';
 
 export const ApiRouter = (app: App) =>
+    // Add the api/v1 group layer. This will enable backwards compatibility
+    // if we ever want to change the api schema later.
+    // We will use the `render.io` url rewrite feature to redirect
+    // all non `/` request to `api/v1`
     app.group('api/v1', (app) =>
         app
+            // Use the JSend middleware to return data in JSend format
             .use(JSend())
+            // Add the HealthController, that returns data on the health
+            // of the server and it's connections.
             .use(HealthController)
+            // Add the CreateURLController
             .use(CreateUrlController)
-            .use(ShortUrlController)
-            .use((app) => app),
+            // Add the ShortURLController
+            .use(ShortUrlController),
     );
