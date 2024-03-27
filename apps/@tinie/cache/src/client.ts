555c041e93b7938c2776a56a90b84d6da37a19d0
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-27 10:45:12 +0100

    feat: implement basic redis client error handling

--- apps/@tinie/cache/src/client.ts
+++ apps/@tinie/cache/src/client.ts
@@ -5,3 +5,14 @@ import { Redis } from 'ioredis';
 
 const connect = process.env.REDIS_CONNECT;
 export const client = new Redis(connect);
+
+let retry = 0;
+client.on('error', () => {
+    retry++;
+    console.log(`Could not connect to Redis cache. Attempt: ${retry}`);
+
+    if (retry > 10) {
+        console.log(`Disconnecting from cache.`);
+        client.disconnect();
+    }
+});
