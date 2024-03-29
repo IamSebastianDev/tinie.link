fdfbaa4b216801fcd0103d898162fe221a9935ad
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-29 12:36:34 +0100

    chore: configure vite to provide a useable build and preview output

--- apps/@tinie-client/vite.config.ts
+++ apps/@tinie-client/vite.config.ts
@@ -1,7 +1,18 @@
 /** @format */
 
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
     root: './src/pages/',
+    build: {
+        emptyOutDir: true,
+        outDir: resolve(import.meta.dirname, './dist'),
+        rollupOptions: {
+            input: {
+                main: resolve(import.meta.dirname, './src/pages/index.html'),
+                'not-found': resolve(import.meta.dirname, './src/pages/not-found/index.html'),
+            },
+        },
+    },
 });
