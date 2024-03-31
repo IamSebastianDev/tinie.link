944fd779563d264f91d83de34ab3376656c6c92a
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-03-31 23:43:32 +0200

    chore: integrate tailwind

---
+++ apps/@tinie-client/tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+export default {
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
