b7b7706ddca64cd5eb532db8dde7103157190f4c
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-03 23:04:14 +0200

    feat: add footer

--- apps/@tinie-client/src/lib/components/footer/footer.component.ts
+++ apps/@tinie-client/src/lib/components/footer/footer.component.ts
@@ -5,8 +5,15 @@ import { Link } from '../ui/Link/Link.component';
 
 export const Footer = createComponent((html) => {
     const year = new Date().getFullYear();
+    const classes = 'text-zinc-500 font-normal';
 
-    return html`<footer class="w-full p-4 bg-zinc-950 text-zinc-300 text-sm flex justify-center">
-        <div class="flex gap-1">Built with 🫶 - ${year}</div>
+    return html`<footer
+        class="w-full p-4 bg-zinc-950 text-zinc-500 text-sm flex flex-col justify-between gap-2 text-center"
+    >
+        <div>
+            Built with ❤️, ${Link({ label: 'TailwindCSS', href: '', classes })} &
+            ${Link({ label: 'Nørd', href: '', classes })}
+        </div>
+        <div>&copy; ${year} ${Link({ label: 'Sebastian Heinz', href: '', classes })}</div>
     </footer>`;
 });
