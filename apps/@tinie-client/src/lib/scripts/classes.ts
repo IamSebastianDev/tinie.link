7f247b49c936e44a7788b207e836c488b817dca2
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-03 23:04:35 +0200

    fix: join classnames with space instead of comma

--- apps/@tinie-client/src/lib/scripts/classes.ts
+++ apps/@tinie-client/src/lib/scripts/classes.ts
@@ -1,5 +1,8 @@
 /** @format */
 
 export const classes = (...segments: (string | string[] | null | undefined)[]) => {
-    return segments.flat().filter((elem) => !!elem);
+    return segments
+        .flat()
+        .filter((elem) => !!elem)
+        .join(' ');
 };
