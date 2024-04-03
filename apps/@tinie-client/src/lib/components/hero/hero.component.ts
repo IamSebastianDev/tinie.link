08641d3eb91048e25ffe31f85f025718062bcfa3
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-03 17:50:11 +0200

    feat: implement full size hero component

--- apps/@tinie-client/src/lib/components/hero/hero.component.ts
+++ apps/@tinie-client/src/lib/components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 /** @format */
 
 import { createComponent } from '@grainular/nord';
+import background from '../../../assets/images/hero.webp';
 
 export const Hero = createComponent((html) => {
-    return html`<div class="h-screen bg-zinc-900 grid-rows-3 w-full grid">
-        <img class="absolute object-cover w-full h-full" src="https://picsum.photos/1600/1200" />
+    return html`<div class="h-screen bg-gray-200 dark:bg-gray-900 grid-rows-3 w-full grid">
+        <img class="absolute object-cover w-full h-full object-top" src="${background}" />
     </div>`;
 });
