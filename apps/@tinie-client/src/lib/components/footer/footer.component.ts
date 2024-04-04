165b12dca606817c4a0c209a5024da9d148f7e11
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 00:47:31 +0200

    refactor: update footer styling

--- apps/@tinie-client/src/lib/components/footer/footer.component.ts
+++ apps/@tinie-client/src/lib/components/footer/footer.component.ts
@@ -1,14 +1,14 @@
 /** @format */
 
 import { createComponent } from '@grainular/nord';
-import { Link } from '../ui/Link/Link.component';
+import { Link } from '../ui/link/link.component';
 
 export const Footer = createComponent((html) => {
     const year = new Date().getFullYear();
     const classes = 'text-zinc-500 font-normal';
 
     return html`<footer
-        class="w-full p-4 bg-zinc-950 text-zinc-500 text-sm flex flex-col justify-between gap-2 text-center"
+        class="w-full p-4 bg-zinc-950 text-zinc-500 text-sm flex flex-col justify-between gap-2 text-center border-t border-zinc-800"
     >
         <div>
             Built with ❤️, ${Link({ label: 'TailwindCSS', href: 'https://tailwindcss.com', classes, target: '_blank' })}
