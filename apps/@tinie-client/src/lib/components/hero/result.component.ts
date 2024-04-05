383a0825ab884c2a26290f502849a51968fc2f3c
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 08:59:28 +0200

    refactor: improve styling

--- apps/@tinie-client/src/lib/components/hero/result.component.ts
+++ apps/@tinie-client/src/lib/components/hero/result.component.ts
@@ -4,34 +4,38 @@ import { createComponent } from '@grainular/nord';
 import { UrlModel } from '@tinie/models';
 import { Button } from '../ui/button/button.component';
 import copy from '../../../assets/images/copy.svg';
+import { environment } from '../../../pages/env';
 
 export const Result = createComponent<{ url: UrlModel }>((html, { url }) => {
     const copyEntry = async () => {
         // Copy value to clipboard
         try {
-            await navigator.clipboard.writeText(`https://tinie.link/${url.short_url}`);
+            await navigator.clipboard.writeText(`${environment.base_url}/${url.short_url}`);
         } catch (e) {
             console.log(e);
         }
     };
 
-    return html`<div class="text-zinc-200 w-full flex flex-col p-3 gap-4">
-        <div>Success! Copy your shortened URL below.</div>
+    return html`<div class="text-zinc-200 max-w-lg flex flex-col py-3 gap-6">
+        <div>Success! You can copy your created URL below 🫶</div>
         <div
-            class="bg-zinc-300 border border-zinc-700 text-zinc-800 py-1 pl-3 pr-1 rounded-lg flex justify-between items-center"
+            class="bg-zinc-300 border borderx^-zinc-700 text-zinc-800 py-1 pl-3 pr-1 rounded-lg flex justify-between items-center"
         >
-            <span>https://tinie.link/${url.short_url}</span>
+            <span>${environment.base_url}/${url.short_url}</span>
             ${Button(
-                { onClick: () => copyEntry() },
-                (html) =>
-                    html`<div class="rounded-lg bg-sky-800  flex-shrink-0 p-2 group-hover:bg-sky-600">
-                        <img src="${copy}" class="w-4 h-4" />
-                    </div>`,
+                {
+                    onClick: () => copyEntry(),
+                    classes: 'rounded-lg bg-sky-800 flex-shrink-0 p-2 hover:bg-sky-600 text-zinc-300 active:bg-sky-400',
+                },
+                (html) => html`
+                    Copy
+                    <img src="${copy}" class="w-4 h-4" />
+                `,
             )}
         </div>
         <div class="text-zinc-400 text-sm">
-            The created URL will only be saved in your browser, once you forget or delete it, there is no way to
-            retrieve it. Make sure to write it down or save it somewhere.
+            The created URL will only be saved in your browser, once you forget or delete it, there is no way for you to
+            find it again. Make sure to write it down or save it somewhere else.
         </div>
     </div>`;
 });
--- apps/@tinie-client/src/lib/components/popup/popup-outlet.ts
+++ apps/@tinie-client/src/lib/components/popup/popup-outlet.ts
@@ -8,14 +8,15 @@ export const PopupOutlet = createComponent((html) => {
     return html`<div id="popup-outlet">
         ${when(popupService.popup, (v) => !!v).then(
             html`<div class="fixed inset-0 bg-zinc-900 bg-opacity-90 grid place-items-center z-30">
-                <div class="relative bg-zinc-900 border-2 border-zinc-700 px-2 w-[30vw] min-w-80 rounded-lg">
+                <div class="bg-zinc-900 border-2 border-zinc-700 px-4 py-2 max-w-xl min-w-80 rounded-lg">
                     <!-- Content -->
                     <div id="popup-content" class="flex flex-col"></div>
                     <!-- Controls -->
-                    <div class="flex gap-2 justify-end border-t border-zinc-700 px-3 py-2 pt-1.5 mt-2">
+                    <div class="border-t border-zinc-700 pt-2 mt-2">
                         ${Button({
                             label: 'Close',
-                            classes: 'text-zinc-300 hover:text-amber-600',
+                            classes:
+                                'text-zinc-200 hover:bg-sky-600 w-full text-center rounded-lg bg-zinc-800 py-2 px-5',
                             onClick: () => popupService.close(),
                         })}
                     </div>
