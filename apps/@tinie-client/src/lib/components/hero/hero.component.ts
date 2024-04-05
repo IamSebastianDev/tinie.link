72fcf072869b170d835fc9e61506d70b6a170281
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 10:29:11 +0200

    refactor: move hero content to main

--- apps/@tinie-client/src/lib/components/hero/hero.component.ts
+++ apps/@tinie-client/src/lib/components/hero/hero.component.ts
@@ -2,39 +2,12 @@
 
 import { createComponent } from '@grainular/nord';
 import background from '../../../assets/images/hero.webp';
-import { Input } from './input.component';
-import { fetchShortUrlService } from '../../services/fetch-short-url.service';
 import { shrinkHero } from '../../directives/shrink-hero.directive';
-import { popupService } from '../../services/popup.service';
-import { Result } from './result.component';
 
-export const Hero = createComponent((html) => {
-    const onSubmit = (short: string) => {
-        fetchShortUrlService.fetchShortUrl(short).subscribe((result) => {
-            if (result) {
-                popupService.open(Result, { url: result! });
-            }
-        });
-    };
-
+export const Hero = createComponent((html, { $children }) => {
     return html`<div class="hero relative bg-zinc-950 w-full py-16" ${shrinkHero('h-screen')}>
         <img class="absolute object-cover w-full h-full object-top" src="${background}" />
-        <!-- Heading -->
-        <div
-            class="self-center z-10 flex flex-col justify-center items-center max-w-7xl w-full m-auto text-center px-10 max-xl:max-w-4xl gap-5"
-        >
-            <h1
-                class="font-bold py-4 text-7xl max-xl:text-5xl bg-gradient-to-r from-red-600 via-amber-400 to-red-600 inline-block text-transparent bg-clip-text"
-            >
-                Life is Too Short for Long Links
-            </h1>
-            <h3 class="text-zinc-200 tracking-wider text-lg max-xl:text-sm font-semibold font-quicksand">
-                With Tinie, enjoy open-source, cloud-powered link sharing: always free, always private, incredibly fast.
-                🚀
-            </h3>
-        </div>
-        <!-- Input -->
-        <div class="z-10 self-center">${Input({ onSubmit })}</div>
+        ${$children}
     </div>`;
 });
 
--- apps/@tinie-client/src/pages/main.ts
+++ apps/@tinie-client/src/pages/main.ts
@@ -12,13 +12,43 @@ import { Hero } from '../lib/components/hero/hero.component';
 import { Footer } from '../lib/components/footer/footer.component';
 import { List } from '../lib/components/list/list.component';
 import { PopupOutlet } from '../lib/components/popup/popup-outlet';
+import { Input } from '../lib/components/shortening/input.component';
+import { Result } from '../lib/components/shortening/result.component';
+import { fetchShortUrlService } from '../lib/services/fetch-short-url.service';
+import { popupService } from '../lib/services/popup.service';
 
 const App = createComponent((html) => {
+    const onSubmit = (short: string) => {
+        fetchShortUrlService.fetchShortUrl(short).subscribe((result) => {
+            if (result) {
+                popupService.open(Result, { url: result! });
+            }
+        });
+    };
+
     return html`
         <!-- Navigation Components -->
         ${TopNavigation({})}
         <!-- Hero Components -->
-        ${Hero({})}
+        ${Hero(
+            {},
+            (html) =>
+                html` <div
+                        class="self-center z-10 flex flex-col justify-center items-center max-w-7xl w-full m-auto text-center px-10 max-xl:max-w-4xl gap-5"
+                    >
+                        <h1
+                            class="font-bold py-4 text-7xl bg-gradient-to-r from-red-600 via-amber-400 to-red-600 text-transparent bg-clip-text"
+                        >
+                            Life is Too Short for Long Links
+                        </h1>
+                        <h3 class="text-zinc-200 tracking-wider text-lg max-xl:text-sm font-semibold">
+                            With Tinie, enjoy open-source, cloud-powered link sharing: always free, always private,
+                            incredibly fast. 🚀
+                        </h3>
+                    </div>
+                    <!-- Input -->
+                    <div class="z-10 self-center">${Input({ onSubmit })}</div>`,
+        )}
         <!-- List -->
         ${List({})}
         <!-- Footer Component -->
