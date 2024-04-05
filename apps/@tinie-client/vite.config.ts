8ebb1daefd5954fe3f3a28d2ee3fa6eee08eb361
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 08:58:49 +0200

    chore: enable development rewrite and redirect

--- apps/@tinie-client/vite.config.ts
+++ apps/@tinie-client/vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api/v1/': 'http://localhost:3000',
-        },
-    },
     root: './src/pages/',
     build: {
         emptyOutDir: true,
@@ -17,6 +12,16 @@ export default defineConfig({
             input: {
                 main: resolve(import.meta.dirname, './src/pages/index.html'),
                 'not-found': resolve(import.meta.dirname, './src/pages/not-found/index.html'),
+            },
+        },
+    },
+    server: {
+        strictPort: true,
+        proxy: {
+            '/api/v1/': 'http://localhost:3000',
+            '^/[A-Za-z0-9]{7}': {
+                target: 'http://localhost:3000',
+                rewrite: (path) => `/api/v1/short-url/${path}`,
             },
         },
     },
