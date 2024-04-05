3bece86af302b749275be051064bc60578a74784
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 15:46:02 +0200

    feat: implement server side rate limiting

--- apps/@tinie-server/src/bootstrap.ts
+++ apps/@tinie-server/src/bootstrap.ts
@@ -8,6 +8,7 @@ import { helmet } from 'elysia-helmet';
 import { loq } from '../../../packages/@elysia-plugin-loq';
 import { documentation } from '@elysia-plugin/scalar';
 import { resolve, join } from 'node:path';
+import { rateLimit } from 'elysia-rate-limit';
 
 export const app = new Elysia()
     // Add request id here so that app retains it's derived type
@@ -29,13 +30,20 @@ export const app = new Elysia()
 // `compression()` will compress responses
 // `helmet()` add certain security parameters and headers to the chain
 // `loq()` is a custom middleware to log requests
-app.use(staticPlugin({ assets: './public/assets', prefix: '/assets', ignorePatterns: [/\.ico/gim] }))
-    .get('/', () => Bun.file(resolve(join(process.cwd(), './public/index.html'))))
-    .get('/404', () => Bun.file(resolve(join(process.cwd(), './public/not-found/index.html'))))
-    .use(documentation())
+app.use(
+    rateLimit({
+        skip: (request, key) => {
+            return request.url.includes('/assets/');
+        },
+    }),
+)
+    .use(staticPlugin({ assets: './public/assets', prefix: '/assets', ignorePatterns: [/\.ico/gim] }))
     .use(cors())
     .use(compression())
-    // .use(helmet())
-    .use(loq());
+    //.use(helmet()) // Helmet appears to cause some issues with scalar, so we skip it for now
+    .use(loq())
+    .use(documentation())
+    .get('/', () => Bun.file(resolve(join(process.cwd(), './public/index.html'))))
+    .get('/404', () => Bun.file(resolve(join(process.cwd(), './public/not-found/index.html'))));
 
 export type App = typeof app;
