f522fbff6d43654f600214e878141abec466e28d
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 15:25:55 +0200

    fix: resolve type issue

--- packages/@tinie-dynamo/src/functions/get-db-status.function.ts
+++ packages/@tinie-dynamo/src/functions/get-db-status.function.ts
@@ -1,17 +1,20 @@
 /** @format */
 
-import { ListTablesCommand } from '@aws-sdk/client-dynamodb';
+import { ListTablesCommand, type ListTablesCommandOutput } from '@aws-sdk/client-dynamodb';
 import { handleAsync } from '@iasd/handle-async';
 import { client } from '../client';
 
 export const getDbStatus = async () => {
-    return handleAsync(
+    return handleAsync<ListTablesCommandOutput, boolean>(
         async () => {
             return client.send(new ListTablesCommand());
         },
         {
-            Ok: ({ TableNames }) => TableNames?.includes('url'),
-            Err: () => false,
+            Ok: ({ TableNames }) => !!TableNames?.includes('url'),
+            Err: (err) => {
+                console.log({ err });
+                return false;
+            },
         },
     );
 };
