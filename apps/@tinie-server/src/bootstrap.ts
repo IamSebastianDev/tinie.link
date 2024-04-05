2e02e85d8837e572e707ae75fa1ef3dbfc2b287f
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-05 14:25:48 +0200

    feat: add static file paths

--- apps/@tinie-server/src/bootstrap.ts
+++ apps/@tinie-server/src/bootstrap.ts
@@ -6,8 +6,8 @@ import { cors } from '@elysiajs/cors';
 import { compression } from 'elysia-compression';
 import { helmet } from 'elysia-helmet';
 import { loq } from '../../../packages/@elysia-plugin-loq';
-import swagger from '@elysiajs/swagger';
 import { documentation } from '@elysia-plugin/scalar';
+import { resolve, join } from 'node:path';
 
 export const app = new Elysia()
     // Add request id here so that app retains it's derived type
@@ -29,7 +29,9 @@ export const app = new Elysia()
 // `compression()` will compress responses
 // `helmet()` add certain security parameters and headers to the chain
 // `loq()` is a custom middleware to log requests
-app.use(staticPlugin({ assets: './assets', prefix: 'assets', ignorePatterns: [/\.ico/gim] }))
+app.use(staticPlugin({ assets: './public/assets', prefix: '/assets', ignorePatterns: [/\.ico/gim] }))
+    .get('/', () => Bun.file(resolve(join(process.cwd(), './public/index.html'))))
+    .get('/404', () => Bun.file(resolve(join(process.cwd(), './public/not-found/index.html'))))
     .use(documentation())
     .use(cors())
     .use(compression())
